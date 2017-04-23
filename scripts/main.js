d86052a566f6f779b9d1d8be40dca4a3e7151cdd
Jaroslaw Tyma <tyma.jaroslaw@gmail.com>
2017-04-23 23:37:30 +0200

    moved global modules loading

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,11 @@ requirejs.config({
     baseUrl: 'scripts/engine',
 });
 
-//add global modules
-require(['utils', 'keyMap', 'log'], function(){
-	console.log('global modules loaded');
-});
-
-//load game and world classes
-require(['../domReady!', 'game', 'worlds/inputTest'], function(dom, Game, World) {
-	console.log('game and world modules loaded');
+require(['../domReady!', 'game', 'worlds/inputTest', 
+	'utils', 'keyMap', 'log'], 
+	function(dom, Game, World) {
+		console.log('all modules loaded');
 	
-	var game = new Game('game_canvas', new World());
-	game.start();
+		var game = new Game('game_canvas', new World());
+		game.start();
 });
