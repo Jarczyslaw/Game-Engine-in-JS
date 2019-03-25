e34a94bf0175bbe50674c4ab26a2ad7ee4abfb73
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-03-25 21:23:37 +0100

    project refresh

--- engine/main.js
+++ engine/main.js
@@ -1,20 +1,14 @@
-// if sceneToLoad is not defined, load default scene
 var scene = 'asteroids';
 if (typeof sceneToLoad !== 'undefined')
 	scene = sceneToLoad;
 
 requirejs.config({
-    baseUrl: 'engine',
-	paths: {
-		components: 'components',
-		commons: 'components/commons',
-		scenes: '../scenes'
-	}
+	baseUrl: 'engine'
 });
 
-require(['domReady!', 'components/game', 'scenes/' + scene,  
-	'components/utils', 'components/keyMap', 'components/log'], 
-	function(dom, Game, Scene) {
+require(['requirejs/domReady!', 'components/game', '../scenes/' + scene,
+	'components/utils', 'components/keyMap', 'components/log'],
+	function (dom, Game, Scene) {
 		console.log('all modules loaded');
 		var game = new Game('game_canvas', new Scene());
 		game.start();
