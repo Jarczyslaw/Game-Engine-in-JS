5e806f398b3630b46f1d3a8bbdc5b8ae3edde0f6
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2019-03-26 19:03:08 +0100

    road to ES6

--- engine/components/log.js
+++ engine/components/log.js
@@ -1,93 +1,92 @@
 define(function () {
 
-	function ConsoleLog() {
+	class ConsoleLog {
+		constructor() {
+			this.info = function (message) {
+				console.log(message);
+			};
 
-		this.info = function (message) {
-			console.log(message);
-		}
+			this.error = function (message) {
+				console.error(message);
+			};
 
-		this.error = function (message) {
-			console.error(message);
-		}
-
-		this.clear = function () {
-			console.clear();
+			this.clear = function () {
+				console.clear();
+			};
 		}
 	}
 
-	function DivLog() {
+	class DivLog {
+		constructor() {
+			let maxCapacity = 1000;
+			let capacity = 0;
+			let logContainer = document.getElementById('log');
 
-		var maxCapacity = 1000;
-		var capacity = 0;
+			let scrollToBottom = function () {
+				logContainer.scrollTop = logContainer.scrollHeight;
+			};
 
-		var logContainer = document.getElementById('log');
+			let checkCapacity = function () {
+				capacity++;
+				if (capacity > maxCapacity) {
+					capacity = 0;
+					logContainer.innerHTML = "";
+				}
+			};
 
-		var scrollToBottom = function () {
-			logContainer.scrollTop = logContainer.scrollHeight;
-		};
+			this.info = function (message) {
+				if (logContainer == null)
+					return;
+				logContainer.innerHTML += message + '<br />';
+				checkCapacity();
+				scrollToBottom();
+			};
 
-		var checkCapacity = function () {
-			capacity++;
-			if (capacity > maxCapacity) {
-				capacity = 0;
-				logContainer.innerHTML = "";
-			}
+			this.error = function (message) {
+				if (logContainer == null)
+					return;
+				logContainer.innerHTML += '<span style="color:red;">' + message + '</span><br />';
+				checkCapacity();
+				scrollToBottom();
+			};
+
+			this.clear = function () {
+				if (logContainer == null)
+					return;
+				logContainer.innerHTML = '';
+			};
 		}
-
-		this.info = function (message) {
-			if (logContainer == null)
-				return;
-
-			logContainer.innerHTML += message + '<br />';
-			checkCapacity();
-			scrollToBottom();
-		};
-
-		this.error = function (message) {
-			if (logContainer == null)
-				return;
-
-			logContainer.innerHTML += '<span style="color:red;">' + message + '</span><br />';
-			checkCapacity();
-			scrollToBottom();
-		};
-
-		this.clear = function () {
-			if (logContainer == null)
-				return;
-
-			logContainer.innerHTML = '';
-		};
 	}
 
-	function Log() {
+	class Log {
+		constructor() {
+			let consoleLog = new ConsoleLog();
+			let divLog = new DivLog();
 
-		var consoleLog = new ConsoleLog();
-		var divLog = new DivLog();
+			let messageHeader = function (object) {
+				var dt = '[' + getDateTime() + ']';
+				if (object == null)
+					return dt + ': ';
+				else
+					return dt + ' ' + object.constructor.name.toUpperCase() + ': ';
+			};
 
-		var messageHeader = function (object) {
-			var dt = '[' + getDateTime() + ']';
-			if (object == null)
-				return dt + ': ';
-			else
-				return dt + ' ' + object.constructor.name.toUpperCase() + ': ';
-		};
+			this.info = function (message, caller = null) {
+				let msg = messageHeader(caller) + message;
+				consoleLog.info(msg);
+				divLog.info(msg);
+			};
 
-		this.info = function (message, caller = null) {
-			var msg = messageHeader(caller) + message;
-			consoleLog.info(msg);
-			divLog.info(msg);
-		}
+			this.error = function (message, caller = null) {
+				let msg = messageHeader(caller) + message;
+				consoleLog.error(msg);
+				divLog.error(msg);
+			};
 
-		this.error = function (message, caller = null) {
-			var msg = messageHeader(caller) + message;
-			consoleLog.error(msg);
-			divLog.error(msg);
-		}
-
-		this.clear = function () {
-			consoleLog.clear();
-			divLog.clear();
+			this.clear = function () {
+				consoleLog.clear();
+				divLog.clear();
+			};
 		}
 	}
 
